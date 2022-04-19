4f4055c32fe4d97ad4b43db6de71ae14d0583735
Fran Ribeiro <ribeirofrv@gmail.com>
2022-04-19 17:06:49 -0300

    feat: Adiciona componentes básicos e rotas

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        <h2>Not Found 404</h2>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
---
+++ src/pages/ProfileEdit.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProfileEdit extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">
+        <h2>Edit</h2>
+      </div>
+    );
+  }
+}
+
+export default ProfileEdit;
