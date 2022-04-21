6dcec4e9f9e9a9952810113402a1cf5ef0e2f603
Fran Ribeiro <ribeirofrv@gmail.com>
2022-04-20 22:43:59 -0300

    feat(Search): Adiciona Input e Botão de Pesquisa

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,44 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = { search: '' };
+    this.SearchInput = this.SearchInput.bind(this);
+    this.isSearchButtonDisabled = this.isSearchButtonDisabled.bind(this);
+  }
+
+  SearchInput({ target }) {
+    const { value } = target;
+    this.setState({ search: value });
+  }
+
+  isSearchButtonDisabled() {
+    const { search } = this.state;
+    const size = 2;
+
+    if (search.length >= size) return false;
+
+    return true;
+  }
+
   render() {
     return (
       <div data-testid="page-search">
         <Header />
-        <h2>Search</h2>
+        <input
+          data-testid="search-artist-input"
+          onChange={ this.SearchInput }
+        />
+        <button
+          type="submit"
+          data-testid="search-artist-button"
+          onClick={ () => console.log('pesquisar') }
+          disabled={ this.isSearchButtonDisabled() }
+        >
+          Pesquisar
+
+        </button>
       </div>
     );
   }
