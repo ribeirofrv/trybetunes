8a1efb70deef49098e5d1a92e3b07e34617c1567
Fran Ribeiro <ribeirofrv@gmail.com>
2022-05-12 09:54:37 -0300

    fix: Compatibilidade da Função `getMusicsPreview` com o mock

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class MusicCard extends Component {
+  constructor() {
+    super();
+    this.state = { isLoading: false, isChecked: false };
+    this.addFavoriteSong = this.addFavoriteSong.bind(this);
+    this.onFavoriteCheckboxClick = this.onFavoriteCheckboxClick.bind(this);
+  }
+
+  async componentDidMount() {
+    await this.addFavoriteSong;
+    await this.onFavoriteCheckboxClick;
+  }
+
+  async onFavoriteCheckboxClick({ target: { checked } }) {
+    // console.log(checked);
+    this.setState({ isChecked: checked, isLoading: true });
+    await this.addFavoriteSong();
+  }
+
+  async addFavoriteSong() {
+    const { trackName, previewUrl, trackId } = this.props;
+    const { isChecked } = this.state;
+
+    const songDetails = { trackName, previewUrl, trackId };
+
+    this.setState({ isLoading: true });
+    if (isChecked) await addSong(songDetails);
+    this.setState({ isLoading: false });
+  }
+
   render() {
-    const { trackName, previewUrl } = this.props;
-    return (
+    const { trackName, previewUrl, trackId } = this.props;
+    const { isLoading, isChecked } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
       <div className="track-card">
         <span>{trackName}</span>
         <audio data-testid="audio-component" src={ previewUrl } controls>
@@ -14,6 +48,21 @@ class MusicCard extends Component {
           <code>audio</code>
           .
         </audio>
+        <label
+          htmlFor={ `checkbox-music-${trackId}` }
+        >
+          <span>
+            Favorita
+          </span>
+          <input
+            data-testid={ `checkbox-music-${trackId}` }
+            type="checkbox"
+            name="favorite"
+            id={ `checkbox-music-${trackId}` }
+            onChange={ this.onFavoriteCheckboxClick }
+            checked={ isChecked }
+          />
+        </label>
       </div>
     );
   }
@@ -22,6 +71,7 @@ class MusicCard extends Component {
 MusicCard.propTypes = ({
   trackName: PropTypes.string,
   previewUrl: PropTypes.string,
+  trackId: PropTypes.string,
 }).isRequired;
 
 export default MusicCard;
