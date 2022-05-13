d6453ab7c7c24883726dd5b8544d3e1b53019b31
Fran Ribeiro <ribeirofrv@gmail.com>
2022-05-13 01:17:43 -0300

    feat: Adiciona Lista de Músicas Favoritas 😲

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,64 @@
 import React, { Component } from 'react';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
 
 class Favorites extends Component {
+  constructor() {
+    super();
+    this.state = { isLoading: true, favoriteSongs: [] };
+
+    this.favoriteSongs = this.favoriteSongs.bind(this);
+    this.updateFavoriteSongs = this.updateFavoriteSongs.bind(this);
+  }
+
+  async componentDidMount() {
+    await this.favoriteSongs();
+  }
+
+  async favoriteSongs() {
+    const favoriteSongs = await getFavoriteSongs();
+
+    this.setState((prevState) => (
+      { ...prevState, favoriteSongs }
+    ), () => {
+      this.setState({ isLoading: false });
+    });
+  }
+
+  async updateFavoriteSongs() {
+    const favoriteSongs = await getFavoriteSongs();
+    this.setState({ favoriteSongs });
+  }
+
   render() {
-    return (
+    const { isLoading, favoriteSongs } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
       <div data-testid="page-favorites">
         <Header />
-        <h2>Favorites</h2>
+        {favoriteSongs.map((track) => (
+          <>
+            <p data-testid="artist-name">
+              {track.artistName}
+            </p>
+            <p data-testid="album-name">
+              {track.collectionName}
+            </p>
+            <MusicCard
+              key={ track.trackId }
+              trackName={ track.trackName }
+              previewUrl={ track.previewUrl }
+              trackId={ track.trackId }
+              track={ track }
+              favoriteSongs={ favoriteSongs }
+              updateFavoriteSongs={ this.updateFavoriteSongs }
+            />
+
+          </>
+        ))}
       </div>
     );
   }
